8e586a78f95b3ef56d06514e9f45db6b012e986f
hkamran3 <hkamran3@uwo.ca>
2024-11-12 22:58:58 -0500

    Added routes and JWT token stuff

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const authMiddleware = require('./middleware/authenticateMiddleware');
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// for json
+//parse JSON
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.get('/api/protected', authMiddleware, (req, res) => {
+  res.json({
+    message: 'This is a protected route.',
+    user: req.user, // This will contain the user information from the token
+  });
+});
+
+
+
+// Import routes
+const authRoutes = require('./userRoutes/authentication');
+
+// Use routes
+app.use('/api/auth', authRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB:', err));
-
-
-//route
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start the server
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((err) => console.error('Could not connect to MongoDB:', err));
--- userRoutes/authentication.js
+++ userRoutes/authentication.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
     if (!passMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
     //JWT token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user._id, email: user.email}, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
